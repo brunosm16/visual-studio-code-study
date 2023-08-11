7ce86e676060f525046ee963ae22d88d6b09ddb2
brunosm16 <brunoskr23@gmail.com>
2023-08-11 09:37:23 -0300

    feat: 🚀 extract dotenv on debugging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+require('dotenv/config');
+
 const { generateIds } = require('./lib/generate-data');
 const { greetByName } = require('./lib/greet');
+
+// eslint-disable-next-line no-unused-vars
+const environment = process.env.NODE_ENV;
 
 // eslint-disable-next-line no-unused-vars
 const greetMessage = greetByName('Jonh Doe');
